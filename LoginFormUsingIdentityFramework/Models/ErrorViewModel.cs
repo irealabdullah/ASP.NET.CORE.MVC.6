dc09a0849e79070dfbcb67d6fdb85cff3b46a37a
Abdullah Tariq <irealabdullah@outlook.com>
2023-10-12 23:25:56 +0500

    Add project files.

---
+++ LoginFormUsingIdentityFramework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginFormUsingIdentityFramework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
